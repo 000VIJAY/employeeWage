ad39e6cb09fbf7e4a9924378c132d2cf70061bea
000VIJAY <vijay893689@gmail.com>
2022-06-21 10:40:06 +0530

    added Employee is present or absent using random

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
 ﻿Console.WriteLine("Welcome to Employee Wage Computation Program");
+int isFullTime = 1;
+Random rnd = new Random();
+int num = rnd.Next(0, 2);
+if (num == isFullTime)
+{
+    Console.WriteLine("Present");
+}
+else
+    Console.WriteLine("Absent");
