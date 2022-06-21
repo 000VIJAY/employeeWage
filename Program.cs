05cd6d0b78acaf0f3e266e5cc76d427697348442
000VIJAY <vijay893689@gmail.com>
2022-06-21 10:58:54 +0530

    Added Employee Wage per Day

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿Console.WriteLine("Welcome to Employee Wage Computation Program");
-int isFullTime = 1;
+﻿int isFullTime = 1;
+int employeeRatePerHr = 20;
+int employeeHrs = 0;
+int employeeWagePerDay = 0;
 Random rnd = new Random();
 int num = rnd.Next(0, 2);
 if (num == isFullTime)
 {
+    employeeHrs = 8;
     Console.WriteLine("Present");
 }
 else
+{
+    employeeHrs = 0;
     Console.WriteLine("Absent");
+}
+employeeWagePerDay = employeeHrs * employeeRatePerHr;
+Console.WriteLine("employeeWagePerDay : " + employeeWagePerDay);
+
+
+
+
+
